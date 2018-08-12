05d3dc2c3776974973bd11df966f0f66b21ae422
RebGov <becci.govert@gmail.com>
2018-08-12 18:49:54 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+  # get 'students/index'
+  #
+  # get 'students/show'
+  #
+  # get 'students/new'
+  #
+  # get 'students/edit'
+
+  resources :students, only: [:index, :show]
+
 end
